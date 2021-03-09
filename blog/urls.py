b31c650ab4320b50bb7c64f04d77f6606a4444ba
joisemp <joise1124@gmail.com>
2021-03-09 12:43:08 +0530

    almost finished the blog home page and blog detail page. added bootsrap and gave some basic styling

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+#from . import views
+from.views import BlogHomeView, BlogDetailView
 
 urlpatterns = [
-    path('', views.blogHome, name="blogHome"),
+    #path('', views.blogHome, name="blogHome"),
+    path('', BlogHomeView.as_view(), name='bloghome'),
+    path('article/<int:pk>', BlogDetailView.as_view(), name='blogdetail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,15 @@
+from blog.models import Post
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def blogHome(request):
-    return render(request, 'blog_home.html', {})
+#def blogHome(request):
+#    return render(request, 'blog_home.html', {})
+
+class BlogHomeView(ListView):
+    model = Post
+    template_name = 'blog_home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_detail.html'
