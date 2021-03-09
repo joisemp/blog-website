03cbb75c4582d43a3c9b61456caa19e90cb0c83c
joisemp <joise1124@gmail.com>
2021-03-09 14:37:31 +0530

    created a create blog page, not yet styled. also created requirments.txt file

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 #from . import views
-from.views import BlogHomeView, BlogDetailView
-
+from.views import BlogHomeView, BlogDetailView, CreatePostView
 urlpatterns = [
     #path('', views.blogHome, name="blogHome"),
     path('', BlogHomeView.as_view(), name='bloghome'),
-    path('article/<int:pk>', BlogDetailView.as_view(), name='blogdetail')
+    path('article/<int:pk>', BlogDetailView.as_view(), name='blogdetail'),
+    path('createPost/', CreatePostView.as_view(), name='createPost')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from blog.models import Post
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 #def blogHome(request):
@@ -13,3 +13,9 @@ class BlogHomeView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_detail.html'
+
+class CreatePostView(CreateView):
+    model = Post
+    template_name = 'create_blog_post.html'
+    fields = '__all__' # this line of code display all the fields that are defined in models.py
+    # fields = ('title', 'body') # this is to choose the fields to be displayed induvidualy from models.py
