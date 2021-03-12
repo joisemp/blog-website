a90ff2c12ca88a44944e95937a5824386464f32e
joisemp <joise1124@gmail.com>
2021-03-12 23:37:24 +0530

    Added Delete funtionality, problem in redireting to the blog detail page after creating a post and moved all the blog html files other than base.html to Blog folder

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from.views import BlogHomeView, BlogDetailView, CreatePostView, UpdatePostView
+from.views import BlogHomeView, BlogDetailView, CreatePostView, UpdatePostView, DeletePostView
 urlpatterns = [
     #path('', views.blogHome, name="blogHome"),
     path('', BlogHomeView.as_view(), name='bloghome'),
     path('article/<int:pk>', BlogDetailView.as_view(), name='blogdetail'),
     path('createPost/', CreatePostView.as_view(), name='createPost'),
     path('article/edit/<int:pk>/', UpdatePostView.as_view(), name='updatePost'),
+    path('article/<int:pk>/remove/', DeletePostView.as_view(), name='deletePost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,35 @@
 from blog.models import Post
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import CreatePostForm, EditPostForm
-
+from django.urls import reverse_lazy
 #def blogHome(request):
 #    return render(request, 'blog_home.html', {})
 
 class BlogHomeView(ListView):
     model = Post
-    template_name = 'blog_home.html'
+    template_name = 'Blog/blog_home.html'
+    # ordering = ['-id'] # This is to order the post by id but it is not efficient everytime so we can you date and time
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'blog_detail.html'
+    template_name = 'Blog/blog_detail.html'
 
 class CreatePostView(CreateView):
     model = Post
     form_class = CreatePostForm # we imported the form from forms.py file so using this instead of field because we not need those
-    template_name = 'create_blog_post.html'
+    template_name = 'Blog/create_blog_post.html'
     # fields = '__all__' # this line of code display all the fields that are defined in models.py
     # fields = ('title', 'body') # this is to choose the fields to be displayed induvidualy from models.py
 
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditPostForm
-    template_name = 'update_blog_post.html'
+    template_name = 'Blog/update_blog_post.html'
     # fields = ['title', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'Blog/delete_blog_post.html'
+    success_url = reverse_lazy('bloghome')
