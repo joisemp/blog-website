9de3b49b4252e6fa1dcbb0b5c86a957b78ff8033
joisemp <joise1124@gmail.com>
2021-03-21 08:56:39 +0530

    Created basic structure of comments section. Now able to add and delete comment from admin pannel only.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 from django.urls import reverse
 from datetime import datetime, date
 from ckeditor.fields import RichTextField
@@ -78,3 +79,14 @@ def Post_delete(sender, instance, **kwargs):
 def Post_delete(sender, instance, **kwargs):
     # Pass false so FileField doesn't save the model.
     instance.profile_pic.delete(False)
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
